f2f986f8a5620bfc5f4d76e22d61cdba7ae653c5
TranquanOlogn <TranquanOlogn@users.noreply.github.com>
2024-12-11 03:01:10 +0000

    deploy: 4aced4e72c6691a5c33097786d1dd38c3191f079

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: "export",
-    basePath: "/ologn-tech.github.io",
-    trailingSlash: "true",
-};
-
-module.exports = nextConfig;
