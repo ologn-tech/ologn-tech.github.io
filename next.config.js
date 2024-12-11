0058ae2f1c8949d48f079d79956c895fc907c555
TranquanOlogn <quan.tran@ologn.tech>
2024-12-11 14:35:22 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    basePath: '/ologn-tech.github.io',
-    assetPrefix: '/ologn-tech.github.io/',
+    basePath: '',
+    assetPrefix: 'https://github.com/ologn-tech/ologn-tech.github.io/',
     trailingSlash: true,
     images: {
         unoptimized: true
