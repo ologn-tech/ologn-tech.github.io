4b92e856424c6e434e258f97ca01a7f7bf008213
TranquanOlogn <quan.tran@ologn.tech>
2024-12-11 11:07:43 +0700

    Update basePath

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    basePath: "/ologn-tech.github.io",
     trailingSlash: true,
     images: {
         unoptimized: true
